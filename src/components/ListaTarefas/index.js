d179e488c0d1e0ef9299f3a1483473f1179d3044
Arthuramaral100 <arthuramaral57@gmail.com>
2022-10-27 09:13:38 -0300

    Exercícios feitos da aula039-renderização de listas

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -14,7 +14,7 @@ import bin from "../../assets/bin.png";
 export function ListaTarefas() {
   const [lista, setLista] = useState(["Fazer exercícios", "Estudar React"]);
   const [novaTarefa, setNovaTarefa] = useState("");
-
+const [listaFeita, setListaFeita] = useState([])
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
@@ -25,9 +25,20 @@ export function ListaTarefas() {
     setNovaTarefa("");
   };
 
+  const adicionaTarefaEnter = (event) => {
+    if(event.key === "Enter"){
+    const novaLista = [...lista, novaTarefa];
+    setLista(novaLista);
+    setNovaTarefa("");
+    }
+  };
+
+  // ↓ Essa função, ao clicar no 'remover', remove o item da primeira lista e adiciona na lista feita
   const removeTarefa = (tarefa) => {
     const listaFiltrada = lista.filter((item) => item !== tarefa);
-    setLista(listaFiltrada);
+    const listaResolvida = [... listaFeita, tarefa]
+    setLista(listaFiltrada)
+    setListaFeita(listaResolvida);
   };
 
   return (
@@ -37,6 +48,7 @@ export function ListaTarefas() {
           placeholder="Digite aqui uma tarefa"
           value={novaTarefa}
           onChange={onChangeTarefa}
+          onKeyPress={(event) => adicionaTarefaEnter(event)}
         />
         <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
@@ -46,6 +58,18 @@ export function ListaTarefas() {
             return (
               <Tarefa key={index}>
                 <p>{tarefa}</p>
+                <RemoveButton onClick={() => removeTarefa(tarefa)}>
+                  <img src={bin} alt="" width="16px" />
+                </RemoveButton>
+              </Tarefa>
+            );
+          })}
+        </ul>
+        <ul>
+          {listaFeita.map((tarefa, index) => {
+            return (
+              <Tarefa key={index}>
+                <s>{tarefa}</s>
                 <RemoveButton onClick={() => removeTarefa(tarefa)}>
                   <img src={bin} alt="" width="16px" />
                 </RemoveButton>
